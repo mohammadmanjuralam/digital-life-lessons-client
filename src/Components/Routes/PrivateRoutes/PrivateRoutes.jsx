beeec8b5e092dafaa16dc86f04f4a3b5561a7e4d
mohammadmanjuralam <mohammadmanjuralam10@gmail.com>
2025-12-12 21:22:46 +0600

    added private routes

---
+++ src/Components/Routes/PrivateRoutes/PrivateRoutes.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { Navigate, useLocation } from "react-router";
+import useAuth from "../../Controller/useAuth/useAuth";
+
+const PrivateRoutes = ({ children }) => {
+  const { user, loading } = useAuth();
+  const location = useLocation();
+  console.log("i am from private route", user);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <span className="loading loading-bars loading-xl"></span>
+      </div>
+    );
+  }
+  if (!user) {
+    return <Navigate state={location.pathname} to="/login"></Navigate>;
+  }
+  return children;
+};
+
+export default PrivateRoutes;
